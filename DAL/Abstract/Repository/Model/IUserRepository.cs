e643ef74bed62022a049b88cf5e2b6d742d24a80
Karlo Bratko <kbratko@racunarstvo.hr>
2022-06-02 00:48:22 +0200

    feat(db): register procedure generates user ID
    Add procedure for generating user ID. That procedure is used by register procedure.
    Update DAL to work with updated procedures.

--- DAL/Abstract/Repository/Model/IUserRepository.cs
+++ DAL/Abstract/Repository/Model/IUserRepository.cs
@@ -5,6 +5,6 @@ namespace DAL.Abstract.Repository.Model
   public interface IUserRepository : IModelRepository<UserModel, int>
   {
     UserModel Login(string Email, string Password);
-    UserModel Register(string UserID, string FName, string LName, string Email, string Password, bool IsAdmin, int CreatedBy);
+    UserModel Register(string FName, string LName, string Email, string Password, bool IsAdmin);
   }
 }
--- DB/Programmability/Stored Procedures/Users/BL/Register.sql
+++ DB/Programmability/Stored Procedures/Users/BL/Register.sql
@@ -1,11 +1,12 @@
-﻿CREATE PROCEDURE [dbo].[UserRegister] (@UserID AS char(11),
-                                       @FName AS nvarchar(50),
+﻿CREATE PROCEDURE [dbo].[UserRegister] (@FName AS nvarchar(50),
                                        @LName AS nvarchar(50),
                                        @Email AS nvarchar(100),
                                        @Password AS nvarchar(512),
-                                       @IsAdmin AS bit,
-                                       @CreatedBy AS int)
+                                       @IsAdmin AS bit)
 AS BEGIN
+  DECLARE @UserID AS char(11)
+  EXECUTE [dbo].[UserGenerateUserID] @IsAdmin = @IsAdmin, @UserID = @UserID OUTPUT
+
   DECLARE @IsUnique AS int = (
     SELECT ALL
       COUNT(*)
@@ -21,6 +22,14 @@ AS BEGIN
   END
 
   DECLARE @PasswordHash AS nvarchar(512) = CONVERT(nvarchar(512), HASHBYTES('SHA2_512', @Password), 2)
+
+  DECLARE @CreatedBy AS int = (
+    SELECT ALL TOP 1
+      [ID]
+    FROM [dbo].[Users]
+    WHERE [IsAdmin] = 1
+    ORDER BY [ID] ASC
+  )
 
   INSERT INTO [dbo].[Users]
   (
