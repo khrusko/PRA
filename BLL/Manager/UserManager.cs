7109fd8b7c90779835cf13bd8170edfa82bad9c6
Karlo Bratko <kbratko@racunarstvo.hr>
2022-06-04 23:23:49 +0200

    feat(bll): update User manager and projection
    Update UserManager, implement new functions for registration. Implement private function for sending Emails for registration confirmation. Function for sending Emails has to be updated later, currently is in testing state.
    Update UserProjection, add new property for ConfirmationGUID.

--- BLL/Manager/UserManager.cs
+++ BLL/Manager/UserManager.cs
@@ -4,8 +4,13 @@ using DAL.Abstract.Repository;
 using DAL.Abstract.Repository.Model;
 using DAL.Factory;
 using DAL.Model;
+using DAL.Status;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
 
 namespace BLL.Manager
 {
@@ -24,7 +29,8 @@ namespace BLL.Manager
         Password = model.PasswordHash,
         ImagePath = model.ImagePath,
         Address = model.Address,
-        IsAdmin = model.IsAdmin
+        IsAdmin = model.IsAdmin,
+        ConfirmationGUID = model.ConfirmationGUID
       };
 
     public UserProjection GetByID(int ID)
@@ -47,7 +53,45 @@ namespace BLL.Manager
       => Register(projection.FName, projection.LName, projection.Email, projection.Password, projection.IsAdmin);
     public UserProjection Register(string FName, string LName, string Email, string Password, bool IsAdmin)
     {
-      return Project((Repository as IUserRepository).Register(FName, LName, Email, Password, IsAdmin));
+      int ID = (Repository as IUserRepository).Register(FName, LName, Email, Password, IsAdmin);
+      if (ID == 0) return null;
+
+      UserProjection projection = GetByID(ID);
+      SendEmailToUser(projection.Email, projection.ConfirmationGUID);
+
+      return projection;
+    }
+
+    public RegistrationStatus CheckRegistrationStatus(Guid ConfirmationGUID)
+      => (Repository as IUserRepository).CheckRegistrationStatus(ConfirmationGUID);
+
+    public void ConfirmRegistration(Guid ConfirmationGUID)
+      => (Repository as IUserRepository).ConfirmRegistration(ConfirmationGUID);
+
+    private void SendEmailToUser(string Email, Guid ConfirmationGUID)
+    {
+      string verificationLink = $"/Registration/UserVerification/{ConfirmationGUID}";
+      string link = HttpContext.Current.Request.Url.AbsoluteUri.Replace(HttpContext.Current.Request.Url.PathAndQuery, verificationLink);
+
+      MailAddress from = new MailAddress("info.knjizarajez@gmail.com", "Knjižara Jež");
+      string password = "Jez@22#Gmail!";
+      MailAddress to = new MailAddress(Email);
+
+      SmtpClient smtp = new SmtpClient();
+      smtp.Host = "smtp.gmail.com";
+      smtp.Port = 587;
+      smtp.EnableSsl = true;
+      smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+      smtp.UseDefaultCredentials = false;
+      smtp.Credentials = new NetworkCredential(from.Address, password);
+
+      MailMessage message = new MailMessage(from, to);
+      message.Subject = "Potvrda registracije";
+      message.Body = "<br/>Uspješno ste se registrirali." +
+                     "<br/>Molimo Vas da potvrdite registraciju klikom na link:" +
+                     "<br/><br/><a href=" + link + ">" + link + "</a>";
+      message.IsBodyHtml = true;
+      smtp.Send(message);
     }
   }
 }
