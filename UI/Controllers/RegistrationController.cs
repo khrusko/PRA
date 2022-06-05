8bd9d4c21dc1664014015c7b53ee0f2cf6c1b9b9
Karlo Bratko <kbratko@racunarstvo.hr>
2022-06-06 01:10:56 +0200

    feat(ui): update UserVerification action in Registration controller

--- UI/Controllers/RegistrationController.cs
+++ UI/Controllers/RegistrationController.cs
@@ -1,6 +1,7 @@
 ﻿using BLL.Abstract.Manager.Projection;
 using BLL.Manager;
 using BLL.Projection;
+using DAL.Status;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,28 @@ namespace UI.Controllers
     public class RegistrationController : Controller
     {
         private readonly IUserManager _userManager = new UserManager();
-        // GET: Registration
-        public ActionResult UserVerification()
+
+
+        [HttpGet]
+        public ActionResult UserVerification(string id)
         {
-            return View();
+            Guid ConfirmationGUID = new Guid(id);
+            RegistrationStatus status = _userManager.CheckRegistrationStatus(ConfirmationGUID);
+            switch (status)
+            {
+              case RegistrationStatus.INVALID:
+                break;
+              case RegistrationStatus.VALID:
+                _userManager.ConfirmRegistration(ConfirmationGUID);
+                break;
+              case RegistrationStatus.APPROVED:
+                break;
+              case RegistrationStatus.TIMEOUT:
+                break;
+              default:
+                break;
+            }
+          return RedirectToAction("Index", "Login");
         }
 
         [HttpGet]
@@ -40,5 +59,5 @@ namespace UI.Controllers
         {
             return View();
         }
-    }
+  }
 }
