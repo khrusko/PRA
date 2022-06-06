853fd178bcb5490ff3dbd226e3f04110581b60f2
khrusko <khrusko@racunarstvo.hr>
2022-06-06 21:55:18 +0200

    feat(ui): update Login
    Delete login from UserController and User views, add Login View Model, add layout to Login View

---
+++ UI/Models/LoginVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    public class LoginVM
+    {
+
+        [Display(Name = "E-mail")]
+        [Required(ErrorMessage = "E-mail je obavezan")]
+        [EmailAddress (ErrorMessage ="Nije valjani E-mail")]
+        public string Email { get; set; }
+
+        [Display(Name = "Zaporka")]
+        [Required(ErrorMessage = "Zaporka je obavezna")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+    }
+}
