61c4ac97d0cae59bf33ac78ead219033ab229243
Karlo Bratko <kbratko@racunarstvo.hr>
2022-06-07 01:09:40 +0200

    feat(db): create procedures for password reset
    Create proceduress for password reset functionallity:
    - RequestResetPassword sets [RequestResetPasswordIsApproved] column to 1
    - CheckResetPasswordStatus checks if reset is valid
    - ReadByEmail reads User by Email (Email is unique column to undeleted Users)
    - ResetPassword resets current password to given password
    - DisableTimeoutResetPassword is job specific procedure which deletes timeout password resets (timeout is 5 minutes)

---
+++ DB/Programmability/Stored Procedures/Users/Jobs/DisableTimeoutResetPassword.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[UserDisableTimeoutResetPassword]
+AS BEGIN
+  UPDATE [dbo].[Users]
+  SET
+    [ResetPasswordIsApproved] = 0
+  WHERE [ResetPasswordIsApproved] = 1 AND
+        DATEDIFF(MINUTE, [ResetPasswordDate], GETDATE()) > 5
+END
+GO
