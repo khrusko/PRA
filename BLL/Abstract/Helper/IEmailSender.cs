b9b664a5012a61a3ad11b84912bfb372ded616a7
Karlo Bratko <kbratko@racunarstvo.hr>
2022-06-06 22:13:56 +0200

    feat(bll): EmailSender and AppConfigHelper classes
    Implement EmailSender class for abstracting away email sending process.
    Implement AppConfigHelper class for getting data from app.config file.

---
+++ BLL/Abstract/Helper/IEmailSender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Abstract.Helper
+{
+  public interface IEmailSender
+  {
+    MailAddress From { get; set; }
+    MailAddress To { get; set; }
+    SmtpClient SmtpClient { get; set; }
+    void SendEmail();
+    void SendEmail(string subject, string body);
+  }
+}
---
+++ BLL/Helper/EmailSender.cs
@@ -0,0 +1,46 @@
+﻿using BLL.Abstract.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Helper
+{
+  public class EmailSender : IEmailSender
+  {
+    public MailAddress From { get; set; } = AppSettingsHelper.From;
+    public MailAddress To { get; set; }
+    public SmtpClient SmtpClient { get; set; } = AppSettingsHelper.SmtpClient;
+    public MailMessage Message { get; set; }
+
+    public EmailSender(MailAddress to)
+    {
+      To = to;
+    }
+    public EmailSender(MailAddress to, MailMessage message) : this(to)
+    {
+      Message = message;
+    }
+    public EmailSender(MailAddress from, MailAddress to, MailMessage message) : this(to, message)
+    {
+      From = from;
+    }
+
+    public void SendEmail()
+    {
+      SmtpClient.Send(Message);
+    }
+    public void SendEmail(string subject, string body)
+    {
+      Message = new MailMessage(From, To)
+      {
+        Subject = subject,
+        Body = body,
+        IsBodyHtml = true
+      };
+      SendEmail();
+    }
+  }
+}
