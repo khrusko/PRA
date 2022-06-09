2d9ec84288d59deb0ce68e255682a0a4e86c395b
Karlo Hruškovec <khrusko@racunarstvo.hr>
2022-06-09 21:17:18 +0200

    feat(ui): add error handling, update book views, add contact page
    Add HttpErrorsController and its views, Add Contact page and redirect to contact page, update BookController

--- UI/Controllers/BookController.cs
+++ UI/Controllers/BookController.cs
@@ -11,9 +11,10 @@ namespace UI.Controllers
 {
     public class BookController : Controller
     {
-        
+
         // GET: Book
         private readonly IBookManager _bookManager = new BookManager();
+        private readonly IPublisherManager _publisherManager = new PublisherManager();
         public ActionResult Index()
         {
             var allBooks = _bookManager.GetAll().ToList();
@@ -24,5 +25,16 @@ namespace UI.Controllers
             var allBooks = _bookManager.GetAll().ToList();
             return View(allBooks);
         }
+
+        public ActionResult Details(int id)
+        {
+            if (id == 0 ) RedirectToAction(actionName: "BadRequest",controllerName: "HttpErrors");
+            var specificBook = _bookManager.GetByID(id);
+            int publisherFKTemp = specificBook.PublisherFK;
+            ViewBag.Publisher = _publisherManager.GetByID(publisherFKTemp).Name;
+            return View(specificBook);
+        }
+
+
     }
 }
---
+++ UI/Controllers/HttpErrorsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class HttpErrorsController : Controller
+    {
+        // GET: HttpErrors
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+
+        public ActionResult BadRequest()
+        {
+            Response.StatusCode = 400;
+            return View();
+        }
+    }
+}
