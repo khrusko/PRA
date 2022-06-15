0ff1b95f14c0db28a4d737f95e484a2a97b9ec11
Karlo Hruškovec <khrusko@racunarstvo.hr>
2022-06-15 16:06:43 +0200

    feat(ui): add DashboardController update _Navigation, add LoanBookVM
    Add DashboardController, LoanBookVM, Views for dashboard, partial views to differentiate admin and registered user dashboards, update _Navigation

---
+++ UI/Controllers/DashboardController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+using BLL.Abstract.Manager.Projection;
+using BLL.Manager;
+using BLL.Projection;
+
+using UI.Models;
+using UI.Models.Concrete;
+namespace UI.Controllers
+{
+  public class DashboardController : Controller
+  {
+
+    private readonly IBookManager _bookManager = new BookManager();
+    private readonly IMessageManager _messageManager = new MessageManager();
+    private readonly IAuthorManager _authorManager = new AuthorManager();
+    private readonly ILoanManager _loanManager = new LoanManager();
+
+    [HttpGet]
+    public ViewResult Index()
+    {
+      IEnumerable<LoanBookVM> loans = from loan in _loanManager.GetAll()
+                                          join book in _bookManager.GetAll()
+                                            on loan.BookFK equals book.ID
+                                          join author in _authorManager.GetAll()
+                                            on book.PublisherFK equals author.ID
+
+                                          select new LoanBookVM
+                                          {
+                                            Book = book,
+                                            Author = author,
+                                            Loan = loan
+                                          };
+      return View("Index", loans);
+    }
+
+    public ActionResult LoanHistory()
+    {
+      return View();
+    }
+
+    public ActionResult Notifications()
+    {
+      return View();
+    }
+    public ActionResult ShoppingHistory()
+    {
+      return View();
+    }
+
+  }
+}
---
+++ UI/Models/LoanBookVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using BLL.Projection;
+
+namespace UI.Models
+{
+  public class LoanBookVM
+  {
+    public BookProjection Book { get; set; }
+    public LoanProjection Loan { get; set; }
+    public AuthorProjection Author { get; set; }
+  }
+}
